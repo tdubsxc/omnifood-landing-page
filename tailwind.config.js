f5af6c15abb1f5720e5b4c732e9cff230ae93493
tdubsxc <tdubsxc@gmail.com>
2022-01-11 14:35:20 -0500

    Tweaked tw config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -51,6 +51,15 @@ module.exports = {
       100: 100,
       auto: 'auto',
     },
+    borderWidth: {
+      DEFAULT: '1px',
+      0: '0',
+      2: '2px',
+      3: '3px',
+      4: '4px',
+      6: '6px',
+      8: '8px',
+    },
     extend: {
       colors: {
         primary: {
